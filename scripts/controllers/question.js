ac4ec0c88a1ff7f8db8c4ba86f222441570519ab
Jonathan <bejonster@gmail.com>
2015-01-13 09:39:13 +0100

    Update question.js

--- scripts/controllers/question.js
+++ scripts/controllers/question.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('stackoverflowCompanyApp').controller('QuestionCtrl', function ($scope, $http) {
-    $http.get('http://api.stackexchange.com/2.2/users/380863;282073;369491;2013557;3628817;3628847;2211877/questions?order=desc&sort=activity&site=stackoverflow').success(function(data) {
+    $http.get('http://api.stackexchange.com/2.2/users/380863;282073;369491;2013557;3628817;3628847;2211877;979064/questions?order=desc&sort=activity&site=stackoverflow').success(function(data) {
         $scope.questions = data;
     });
 });
