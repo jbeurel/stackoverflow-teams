2573f3843186538e908c3d562a391a93a49ce9b4
Jonathan Beurel <bejonster@gmail.com>
2016-10-03 15:51:03 +0200

    Add Richard Casetta User and fix blocked mixed content

--- scripts/controllers/question.js
+++ scripts/controllers/question.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('stackoverflowCompanyApp').controller('QuestionCtrl', function ($scope, $http) {
-    $http.get('http://api.stackexchange.com/2.2/users/380863;282073;369491;2013557;3628817;3628847;2211877;979064;4402624;4173135;2590861/questions?order=desc&sort=activity&site=stackoverflow').success(function(data) {
+    $http.get('https://api.stackexchange.com/2.2/users/380863;282073;369491;2013557;3628817;3628847;2211877;979064;4402624;4173135;2590861;6915759/questions?order=desc&sort=activity&site=stackoverflow').success(function(data) {
         $scope.questions = data;
     });
 });
