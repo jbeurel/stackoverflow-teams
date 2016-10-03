91ddd1fe7f10ce1b4faef7036c424c6b8a95953d
Jonathan Beurel <bejonster@gmail.com>
2016-10-03 15:50:13 +0200

    Fixed Blocked Mixed content bug

--- scripts/controllers/user.js
+++ scripts/controllers/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('stackoverflowCompanyApp').controller('UserCtrl', function ($scope, $http) {
-    $http.get('http://api.stackexchange.com/2.2/users/380863;282073;369491;2013557;3628817;3628847;2211877;979064;4402624;4173135;2590861;6915759?order=desc&sort=reputation&site=stackoverflow').success(function(data) {
+    $http.get('https://api.stackexchange.com/2.2/users/380863;282073;369491;2013557;3628817;3628847;2211877;979064;4402624;4173135;2590861;6915759?order=desc&sort=reputation&site=stackoverflow').success(function(data) {
         $scope.users = data;
     });
 });
